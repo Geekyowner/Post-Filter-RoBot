2f78291bc9d6dca9cf7e9157bc4f5e7ae0741dd7
Geeky <157978295+Geekyowner@users.noreply.github.com>
2024-05-05 19:41:41 +0530

    Update script.py

--- utils/script.py
+++ utils/script.py
@@ -1,5 +1,5 @@
 class script(object):
-    START = """**Hello Sona {} 🤟**
+    START = """**Hello Sona {} 😍**
    
 ɪ ᴀᴍ ᴘᴏsᴛ ʟɪɴᴋ sᴇᴀʀᴄʜ ʙᴏᴛ. ɪ ᴀᴍ ʙᴇsᴛ ᴄʜᴀɴɴᴇʟ ᴘᴏsᴛ sᴇᴀʀᴄʜ ʙᴏᴛ! 
 ɪ ᴡɪʟʟ ғɪʟᴛᴇʀ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ᴘᴏsᴛs ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴀɴᴅ sᴇɴᴅ ɪᴛ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴄʜᴀᴛ ᴡʜᴇɴ sᴏᴍᴇᴏɴᴇ sᴇᴀʀᴄʜ ɪᴛ."""
@@ -7,24 +7,26 @@ class script(object):
     HELP = """ 📝 ʜᴏᴡ ᴛᴏ ᴜꜱᴇ ᴍᴇ ɪɴ ɢʀᴏᴜᴘ? 👇👇
 
 - ᴀᴅᴅ ᴍᴇ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴀɴᴅ ᴄʜᴀɴɴᴇʟ ᴡɪᴛʜ ᴀʟʟ ᴘᴇʀᴍɪssɪᴏɴs.\n 
-- sᴇɴᴅ /verify ɪɴ ɢʀᴏᴜᴘ ᴀɴᴅ ᴡᴀɪᴛ ғᴏʀ ɪᴛ ᴛᴏ ᴀᴄᴄᴇᴘᴛ ᴏʀ ᴅɪʀᴇᴄᴛʟʏ ᴄᴏɴᴛᴀᴄᴛ ᴛᴏ ᴏᴡɴᴇʀ ᴛᴏ ᴠᴇʀɪꜰʏ ʏᴏᴜʀ ʀᴇQᴜᴇꜱᴛ👉 @MisterBrutal.\n
-- ᴀғᴛᴇʀ ᴠᴇʀɪғɪᴄᴀᴛɪᴏɴ sᴇɴᴅ /connect ʏᴏᴜʀᴄʜᴀɴɴᴇʟɪᴅ \n
+- sᴇɴᴅ /verify ɪɴ ɢʀᴏᴜᴘ ᴀɴᴅ ᴡᴀɪᴛ ғᴏʀ ɪᴛ ᴛᴏ ᴀᴄᴄᴇᴘᴛ\n
+- ᴏʀ ᴅɪʀᴇᴄᴛʟʏ ᴄᴏɴᴛᴀᴄᴛ ᴏᴡɴᴇʀ ᴛᴏ ᴠᴇʀɪꜰʏ ʏᴏᴜʀ ʀᴇQᴜᴇꜱᴛ👉 @MisterBrutal.\n
+- ᴀғᴛᴇʀ ᴠᴇʀɪғɪᴄᴀᴛɪᴏɴ sᴇɴᴅ /connect ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ɪᴅ \n
 - ᴇxᴀᴍᴘʟᴇ : /connect -100xxxxxxxxxx \n
-- ᴀʟsᴏ ʏᴏᴜ ᴄᴀɴ ᴀᴅᴅ ғᴏʀᴄᴇ sᴜʙsᴄʀɪʙᴇ ᴄʜᴀɴɴᴇʟ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ʙʏ /fsub ʏᴏᴜʀᴄʜᴀɴɴᴇʟɪᴅ ᴀɴᴅ ᴄᴀɴ ʀᴇᴍᴏᴠᴇ ʙʏ /nofsub.\n 
-- ᴅᴏɴᴇ ᴇɴɪᴏʏ 🤟
+- ʏᴏᴜ ᴄᴀɴ ᴀᴅᴅ ғᴏʀᴄᴇ sᴜʙsᴄʀɪʙᴇ ᴄʜᴀɴɴᴇʟ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ʙʏ /fsub -100xxxxxxxx (ᴄʜᴀɴɴᴇʟ ɪᴅ) 
+- ʏᴏᴜ ᴄᴀɴ ʀᴇᴍᴏᴠᴇ ғᴏʀᴄᴇ sᴜʙsᴄʀɪʙᴇ ᴄʜᴀɴɴᴇʟ ʙʏ /nofsub -100xxxxxxxxxxx\n 
+- ᴅᴏɴᴇ ᴇɴᴊᴏʏ ɴᴏᴡ 🤟
 
-ʀᴇᴍᴏᴠᴇ ᴀ ᴄʜᴀɴɴᴇʟ ᴡɪᴛʜ - /disconnect -100xxxxxxxxxxx
+ʀᴇᴍᴏᴠᴇ ᴀ ᴄʜᴀɴɴᴇʟ ᴡɪᴛʜ - /disconnect -100xxxxxxxxxxx\n
 ᴛʜɪs ᴡɪʟʟ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ʀᴇᴍᴏᴠᴇ ᴀ ɪɴᴅᴇxᴇᴅ ᴄʜᴀɴɴᴇʟ ғʀᴏᴍ ʏᴏᴜʀ ɢʀᴏᴜᴘ.
 
-ɢᴇᴛ ᴄᴏɴɴᴇᴄᴛᴇᴅ ᴄʜᴀɴɴᴇʟs ʟɪsᴛ ᴡɪᴛʜ - /connection\n 💥 Powered By - @MisterBrutal"""
+ɢᴇᴛ ᴄᴏɴɴᴇᴄᴛᴇᴅ ᴄʜᴀɴɴᴇʟs ʟɪsᴛ ᴡɪᴛʜ - /connection\n 💥 ʙᴏᴛ ᴅᴇᴠᴇʟᴏᴘᴇʀ  - @MisterBrutal"""
 
-    ABOUT = """🔥 Bot Developer/Owner- @MisterBrutal
+    ABOUT = """🔥 ʙᴏᴛ ᴅᴇᴠᴇʟᴏᴘᴇʀ - @MisterBrutal
 
 ✯ Mʏ Nᴀᴍᴇ:  {}
-✯ Cʀᴇᴀᴛᴏʀ: <a href='https://t.me/MisterBrutal'> Mister Brutal</a>
-✯ Lᴀɴɢᴜᴀɢᴇ: <a href='https://www.python.org'>Pʏᴛʜᴏɴ 3</a>
-✯ DᴀᴛᴀBᴀsᴇ: <a href='https://cloud.mongodb.com/'>MᴏɴɢᴏDB</a>
-✯ Bᴏᴛ Sᴇʀᴠᴇʀ: : <a href='https://heroku.com'>Heroku</a>"""
+✯ Cʀᴇᴀᴛᴏʀ: <a href='https://t.me/MisterBrutal'> ᴍɪꜱᴛᴇʀ ʙʀᴜᴛᴀʟ </a>
+✯ ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ: <a href='https://t.me/Movies_Search_Groupp'> ᴊᴏɪɴ ɢʀᴏᴜᴘ </a>
+✯ ᴍᴏᴠɪᴇ ᴄʜᴀɴɴᴇʟ : <a href='https://t.me/Geeky_Network'>ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ </a>
+✯ ʙᴏᴛꜱ ᴄʜᴀɴɴᴇʟ: <a href='https://t.me/MisterBrutal'ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ ></a>"""
 
     STATS = """My Status 💫
 
