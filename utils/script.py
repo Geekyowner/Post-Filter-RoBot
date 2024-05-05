02984e47f3316052b2fbb4aafd01cc0bea739dbf
Geeky <157978295+Geekyowner@users.noreply.github.com>
2024-05-05 19:10:21 +0530

    Update script.py

--- utils/script.py
+++ utils/script.py
@@ -1,27 +1,27 @@
 class script(object):
-    START = """**Hello {} 🤟**
+    START = """**Hello Sona {} 🤟**
    
-ɪ ᴀᴍ ᴘᴏsᴛ ʟɪɴᴋ sᴇᴀʀᴄʜ ʙᴏᴛ.ɪ ᴀᴍ ʙᴇsᴛ ᴄʜᴀɴɴᴇʟ ʟɪɴᴋ sᴇᴀʀᴄʜ ʙᴏᴛ! 
+ɪ ᴀᴍ ᴘᴏsᴛ ʟɪɴᴋ sᴇᴀʀᴄʜ ʙᴏᴛ. ɪ ᴀᴍ ʙᴇsᴛ ᴄʜᴀɴɴᴇʟ ᴘᴏsᴛ sᴇᴀʀᴄʜ ʙᴏᴛ! 
 ɪ ᴡɪʟʟ ғɪʟᴛᴇʀ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ᴘᴏsᴛs ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴀɴᴅ sᴇɴᴅ ɪᴛ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴄʜᴀᴛ ᴡʜᴇɴ sᴏᴍᴇᴏɴᴇ sᴇᴀʀᴄʜ ɪᴛ."""
 
-    HELP = """ᴛᴏ ᴜsᴇ ᴍᴇ ɪɴ ᴀ ɢʀᴏᴜᴘ
+    HELP = """ 📝 ʜᴏᴡ ᴛᴏ ᴜꜱᴇ ᴍᴇ ɪɴ ɢʀᴏᴜᴘ? 👇👇
 
-- ᴀᴅᴅ ᴍᴇ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴀɴᴅ ᴄʜᴀɴɴᴇʟ ᴡɪᴛʜ ᴀʟʟ ᴘᴇʀᴍɪssɪᴏɴs. 
-- sᴇɴᴅ /verify ɪɴ ɢʀᴏᴜᴘ ᴀɴᴅ ᴡᴀɪᴛ ғᴏʀ ɪᴛ ᴛᴏ ᴀᴄᴄᴇᴘᴛ ᴏʀ ᴅɪʀᴇᴄᴛʟʏ ᴄᴏɴᴛᴀᴄᴛ ᴛᴏ ᴏᴡɴᴇʀ ᴀғᴛᴇʀ ʀᴇǫᴜᴇsᴛ @MisterBrutal.
-- ᴀғᴛᴇʀ ᴠᴇʀɪғɪᴄᴀᴛɪᴏɴ sᴇɴᴅ /connect ʏᴏᴜʀᴄʜᴀɴɴᴇʟɪᴅ
-- ᴇxᴀᴍᴘʟᴇ : /connect -100xxxxxxxxxx
-- ᴀʟsᴏ ʏᴏᴜ ᴄᴀɴ ᴀᴅᴅ ғᴏʀᴄᴇ sᴜʙsᴄʀɪʙᴇ ᴄʜᴀɴɴᴇʟ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ʙʏ /fsub ʏᴏᴜʀᴄʜᴀɴɴᴇʟɪᴅ ᴀɴᴅ ᴄᴀɴ ʀᴇᴍᴏᴠᴇ ʙʏ /nofsub.
-- ᴅᴏɴᴇ ᴇɴɪᴏʏ
+- ᴀᴅᴅ ᴍᴇ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴀɴᴅ ᴄʜᴀɴɴᴇʟ ᴡɪᴛʜ ᴀʟʟ ᴘᴇʀᴍɪssɪᴏɴs.\n 
+- sᴇɴᴅ /verify ɪɴ ɢʀᴏᴜᴘ ᴀɴᴅ ᴡᴀɪᴛ ғᴏʀ ɪᴛ ᴛᴏ ᴀᴄᴄᴇᴘᴛ ᴏʀ ᴅɪʀᴇᴄᴛʟʏ ᴄᴏɴᴛᴀᴄᴛ ᴛᴏ ᴏᴡɴᴇʀ ᴛᴏ ᴠᴇʀɪꜰʏ ʏᴏᴜʀ ʀᴇQᴜᴇꜱᴛ👉 @MisterBrutal.\n
+- ᴀғᴛᴇʀ ᴠᴇʀɪғɪᴄᴀᴛɪᴏɴ sᴇɴᴅ /connect ʏᴏᴜʀᴄʜᴀɴɴᴇʟɪᴅ \n
+- ᴇxᴀᴍᴘʟᴇ : /connect -100xxxxxxxxxx \n
+- ᴀʟsᴏ ʏᴏᴜ ᴄᴀɴ ᴀᴅᴅ ғᴏʀᴄᴇ sᴜʙsᴄʀɪʙᴇ ᴄʜᴀɴɴᴇʟ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ʙʏ /fsub ʏᴏᴜʀᴄʜᴀɴɴᴇʟɪᴅ ᴀɴᴅ ᴄᴀɴ ʀᴇᴍᴏᴠᴇ ʙʏ /nofsub.\n 
+- ᴅᴏɴᴇ ᴇɴɪᴏʏ 🤟
 
 ʀᴇᴍᴏᴠᴇ ᴀ ᴄʜᴀɴɴᴇʟ ᴡɪᴛʜ - /disconnect -100xxxxxxxxxxx
 ᴛʜɪs ᴡɪʟʟ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ʀᴇᴍᴏᴠᴇ ᴀ ɪɴᴅᴇxᴇᴅ ᴄʜᴀɴɴᴇʟ ғʀᴏᴍ ʏᴏᴜʀ ɢʀᴏᴜᴘ.
 
-ɢᴇᴛ ᴄᴏɴɴᴇᴄᴛᴇᴅ ᴄʜᴀɴɴᴇʟs ʟɪsᴛ ᴡɪᴛʜ - /connection"""
+ɢᴇᴛ ᴄᴏɴɴᴇᴄᴛᴇᴅ ᴄʜᴀɴɴᴇʟs ʟɪsᴛ ᴡɪᴛʜ - /connection\n 💥 Powered By - @MisterBrutal"""
 
-    ABOUT = """Powered By @MisterBrutal
+    ABOUT = """🔥 Bot Developer/Owner- @MisterBrutal
 
 ✯ Mʏ Nᴀᴍᴇ:  {}
-✯ Cʀᴇᴀᴛᴏʀ: <a href='https://t.me/MisterBrutal'>Brutal</a>
+✯ Cʀᴇᴀᴛᴏʀ: <a href='https://t.me/MisterBrutal'> Mister Brutal</a>
 ✯ Lᴀɴɢᴜᴀɢᴇ: <a href='https://www.python.org'>Pʏᴛʜᴏɴ 3</a>
 ✯ DᴀᴛᴀBᴀsᴇ: <a href='https://cloud.mongodb.com/'>MᴏɴɢᴏDB</a>
 ✯ Bᴏᴛ Sᴇʀᴠᴇʀ: : <a href='https://heroku.com'>Heroku</a>"""
