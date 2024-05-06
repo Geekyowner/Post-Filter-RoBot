16f681ea41892522877de72b5da9ab8484504462
Noob Developer <157978295+Geekyowner@users.noreply.github.com>
2024-05-06 20:00:10 +0530

    Update donate.py

--- plugins/donate.py
+++ plugins/donate.py
@@ -1,1 +1,7 @@
-
+@Client.on_message(filters.private & filters.command(["donate"]))
+async def donatecm(bot,message):
+	text = script.DONATE_TXT
+	keybord = InlineKeyboardMarkup([
+        			[InlineKeyboardButton("🦋 Admin",url = "https://t.me/mrbrutal_141"), 
+        			InlineKeyboardButton("✖️ Close",callback_data = "cancel") ]])
+	await message.reply_text(text = text,reply_markup = keybord)
