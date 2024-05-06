9430a885427fec740954652468b01ee2379fc2ca
Noob Developer <157978295+Geekyowner@users.noreply.github.com>
2024-05-06 21:59:30 +0530

    Delete plugins/donate.py

--- plugins/donate.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
-@Client.on_message(filters.private & filters.command(["donate"]))
-async def donatecm(bot,message):
-	text = script.DONATE
-	keybord = InlineKeyboardMarkup([
-        			[InlineKeyboardButton("🦋 Admin",url = "https://t.me/mrbrutal_141"), 
-        			InlineKeyboardButton("🔙 Back",callback_data = "help") ]])
-	await message.reply_text(text = text,reply_markup = keybord)
